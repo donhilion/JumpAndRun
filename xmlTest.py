8f6b756623e419e21a2f9b7f6a03bcf7f2a1a460
Benjamin Kirchner <benjamin.kirchner@mni.thm.de>
2012-07-17 21:20:34 +0200

    added test for reading and writing xmls

---
+++ xmlTest.py
@@ -0,0 +1,22 @@
+import xml.dom.minidom as dom
+
+d = {} 
+baum = dom.parse("testXml.xml")
+
+for entry in baum.firstChild.childNodes: 
+	if entry.nodeName == "entry": 
+		key = wert = None
+
+		for node in entry.childNodes: 
+			if node.nodeName == "key": 
+				key = eval("%s('%s')" % (node.getAttribute("type"), 
+                              node.firstChild.data.strip()))
+			elif node.nodeName == "value": 
+				value = eval("%s('%s')" % (node.getAttribute("type"), 
+                              node.firstChild.data.strip()))
+
+		d[key] = value
+
+for key in d.keys():
+	print(key)
+	print(d[key]+1)
