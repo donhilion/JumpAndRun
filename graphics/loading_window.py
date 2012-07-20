6429bfb4dad353693bbfc5e77dcce58fbee6bdc6
Benjamin Kirchner <benjamin.kirchner@mni.thm.de>
2012-07-20 11:52:00 +0200

    added the loading of settings

--- graphics/loading_window.py
+++ graphics/loading_window.py
@@ -2,28 +2,40 @@ import pygame
 from pygame.locals import *
 
 from ressources.pictures.picture_manager import PictureManager
+from ressources.settings.settings_manager import SettingsManager
 from ressources.ressource_manager import RessourceWrapper
 
 class LoadingWindow(object):
 	'''
 	'''
+
+	SETTINGS_FILES = ("graphics.xml", )
+	PICTURE_FILES = ("background.png", )
+
+	INTEND = "    "
+
 	def __init__(self):
 		self.screen = pygame.display.set_mode((400, 400), 0, 32)
 		self.font = pygame.font.SysFont("arial", 16)
 		self.font_height = self.font.get_linesize()
 
-
 	def load(self):
 		running = True
 		infos = []
 		results = []
+		ok = True
 
-		# TODO load settings
+		infos.append("Start loading graphics settings")
+		self.settings_manager = SettingsManager()
+		for file_name in LoadingWindow.SETTINGS_FILES:
+			infos.append(LoadingWindow.INTEND + "Start loading " + file_name)
+			results.append(self.settings_manager.load_setting(file_name))
 
 		infos.append("Start loading pictures")
 		manager = PictureManager()
-		wrapper = manager.load_picture("background.png") # TODO use list
-		results.append(wrapper)
+		for file_name in LoadingWindow.PICTURE_FILES:
+			infos.append(LoadingWindow.INTEND + "Start loading " + file_name)
+			results.append(manager.load_picture(file_name))
 
 		while running:
 			for event in pygame.event.get():
@@ -35,16 +47,19 @@ class LoadingWindow(object):
 					infos.append("Done loading " + result.name)
 					results.remove(result)
 				if result.status == RessourceWrapper.FAILED:
+					ok = False
 					infos.append("Error during loading " + result.name)
 					results.remove(result)
 		
-			self.screen.fill((255, 255, 255))
+			self.screen.fill((0, 0, 0))
 
 			line = 0
 			for info in infos:
-				text = self.font.render(info, True, (0, 0, 0))
+				text = self.font.render(info, True, (0, 255, 0))
 				self.screen.blit(text, (8, line))
 				line += self.font_height
 
 			pygame.display.update()
 
+			if len(results) == 0:
+				running = False
