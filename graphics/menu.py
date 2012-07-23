330982532a9073f1969b895cc241d22683377ca6
Benjamin Kirchner <benjamin.kirchner@mni.thm.de>
2012-07-23 17:02:01 +0200

    added menu entries and a simple menu coordination.

--- graphics/menu.py
+++ graphics/menu.py
@@ -4,9 +4,18 @@ import logging
 
 from ressources.pictures.picture_manager import PictureManager
 
+class MenuEntry(object):
+
+	def __init__(self, text, action):
+		self.text = text
+		self.action = action
+
 class Menu(object):
 	'''
 	'''
+
+	ENTRYS = (MenuEntry("Start", None), MenuEntry("Options", None),
+		MenuEntry("Ende", exit))
 
 	def __init__(self, screen, width, height):
 		self.screen = screen
@@ -14,7 +23,34 @@ class Menu(object):
 		self.height = height
 		self.bg = PictureManager.MANAGER.loaded["menuBackground.png"]
 		self.bg = pygame.transform.scale(self.bg, (width, height))
+		self.font = pygame.font.SysFont("arial", 24)
+		self.font_height = self.font.get_linesize()
+		self.selected = 0
 
 	def draw(self):
 		self.screen.blit(self.bg, (0,0))
+		y = 0.5 * (self.height - len(Menu.ENTRYS) * (self.font_height + 5))
+		i = 0
+		for entry in Menu.ENTRYS:
+			if self.selected == i:
+				text = self.font.render(entry.text, True, (255, 255, 0))
+			else:
+				text = self.font.render(entry.text, True, (150, 150, 0))
+			self.screen.blit(text, (8, y)) # TODO center text
+			y += self.font_height + 5
+			i += 1
+
+	def key_down(self, key):
+		if key == K_DOWN and self.selected < len(Menu.ENTRYS) - 1:
+			self.selected += 1
+		elif key == K_UP and self.selected > 0:
+			self.selected -= 1
+		elif key == K_RETURN:
+			entry = Menu.ENTRYS[self.selected]
+			if entry.action is not None:
+				(entry.action)()
+
+
+	def key_up(self, key):
+		pass
 		
--- graphics/window.py
+++ graphics/window.py
@@ -27,14 +27,18 @@ class Window(object):
 			self.height = 400
 		self.screen = pygame.display.set_mode((self.width, self.height), 0, 32)
 		self.menu = Menu(self.screen, self.width, self.height)
+		self.current_display = self.menu
 
 	def start(self):
 		while True:
 			for event in pygame.event.get():
 				if event.type == QUIT:
 					exit() # TODO: check what else has to be done
+				elif event.type == KEYDOWN:
+					self.current_display.key_down(event.key)
+				elif event.type == KEYUP:
+					self.current_display.key_up(event.key)
 
-			#self.screen.fill((0, 0, 0))
-			self.menu.draw()
+			self.current_display.draw()
 
 			pygame.display.update()
