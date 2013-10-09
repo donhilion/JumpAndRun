3bf6cae422868328fe2bcd1e7396fc6bc424e5b4
Donhilion <donhilion@gmail.com>
2013-10-09 11:21:36 +0200

    Commented environment class.

--- graphics/environment.py
+++ graphics/environment.py
@@ -1,21 +1,25 @@
-'''
-Created on 10.07.2012
-
-@author: Donhilion
-'''
+__author__ = 'Donhilion'
 
 from camera import Camera
 
-class GraphicsEnvironment(object):
-	''' The graphics environment.
 
-	This class contains informations about the current graphics environment.
+class GraphicsEnvironment(object):
+	""" The graphics environment.
+
+	This class contains information about the current graphics environment.
 
 	Attributes:
 		camera: The current camera of the environment. This is a Camera.
-	'''
-	
+	"""
+
 	def __init__(self, camera=None):
+		""" Generates a new instance of this class.
+
+		Generates a new instance of this class and sets the field information.
+
+		Args:
+			camera: The camera of this environment.
+		"""
 		if camera is None:
 			camera = Camera()
 		self.camera = camera
