641455af7633abc246341b5766de5be0e9e89807
Donhilion <donhilion@gmail.com>
2013-10-09 10:51:48 +0200

    Commented static class.

--- graphics/drawables/static.py
+++ graphics/drawables/static.py
@@ -1,14 +1,41 @@
 import pygame
 
-class Static(object):
+__author__ = 'Donhilion'
 
-	def __init__(self, picture_manager, frame, repeat=False, width=0, height=0):
+
+class Static(object):
+	""" The static class.
+
+	Class for static object to draw.
+
+	Attributes:
+		_image: The image to draw.
+	"""
+
+	def __init__(self, picture_manager, frame):
+		""" Generates a new instance of this class.
+
+		Generates a new instance of this class and sets the field information.
+
+		Args:
+			picture_manager: The picture_manager to use.
+			frame: The frame information to use.
+		"""
 		picture = picture_manager.loaded[frame[0]]
 		rect = pygame.Rect(float(frame[1]), float(frame[2]),
-			float(frame[3]) - float(frame[1]),
-			float(frame[4]) - float(frame[2]))
-		self.image = picture.subsurface(rect)
+						   float(frame[3]) - float(frame[1]),
+						   float(frame[4]) - float(frame[2]))
+		self._image = picture.subsurface(rect)
 
 
 	def draw(self, surface, x, y):
-		surface.blit(self.image, x, y)
+		""" Draws this static.
+
+		This method draws this static object on the given surface.
+
+		Args:
+			surface: The surface to draw on.
+			x: The x coordinate.
+			y: The y coordinate.
+		"""
+		surface.blit(self._image, x, y)
