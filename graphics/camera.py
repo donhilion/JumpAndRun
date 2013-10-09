eed70a93bbde4c50944fd542120f8c7b15d4bccc
Donhilion <donhilion@gmail.com>
2013-10-09 11:19:21 +0200

    Commented camera class.

--- graphics/camera.py
+++ graphics/camera.py
@@ -1,22 +1,29 @@
-'''
-Created on 10.07.2012
+__author__ = 'Donhilion'
 
-@author: Donhilion
-'''
 
 class Camera(object):
-	''' The camera.
+	""" The camera.
 
-	This class contains informations about the view of a camera.
+	This class contains information about the view of a camera.
 
 	Attributes:
 		x: The x coordinate of the camera. This is a number.
 		y: The y coordinate of the camera. This is a number.
 		width: The width of the camera. This is a number.
 		height: The height of the camera. This is a number.
-	'''
-	
+	"""
+
 	def __init__(self, x=0, y=0, width=0, height=0):
+		""" Generates a new instance of this class.
+
+		Generates a new instance of this class and sets the field information.
+
+		Args:
+			x: The x coordinate of the camera.
+			y: The y coordinate of the camera.
+			width: The width of the camera.
+			height: The height of the camera.
+		"""
 		self.x = x
 		self.y = y
 		self.width = width
