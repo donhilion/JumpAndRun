dd954aaab76da532c5843a5ff9ff134e84d2e243
Donhilion <donhilion@gmail.com>
2013-10-10 16:07:47 +0200

    Commented game over class.

--- graphics/game_over.py
+++ graphics/game_over.py
@@ -4,26 +4,68 @@ import window
 
 __author__ = 'donhilion'
 
-class GameOver(object):
 
+class GameOver(object):
+	""" The game over class.
+
+	An object of this class represents a game over screen.
+
+	Attributes:
+		_screen: The surface to draw on.
+		_width: The width of the window.
+		_height: The height of the window.
+		_window: The surrounding window.
+	"""
+
+	# The text which will be shown.
 	GAME_OVER_TEXT = "Game Over!"
 
 	def __init__(self, screen, width, height, window):
-		self.screen = screen
-		self.width = width
-		self.height = height
-		self.window = window
-		self.font = pygame.font.SysFont("arial", 40)
-		self.font_height = self.font.get_linesize()
+		""" Generates a new instance of this class.
+
+		Generates a new instance of this class and sets the field information.
+
+		Args:
+			screen: The surface of the window.
+			width: The width of the window.
+			height: The height of the window.
+			window: The surrounding window.
+		"""
+		self._screen = screen
+		self._width = width
+		self._height = height
+		self._window = window
+		self._font = pygame.font.SysFont("arial", 40)
+		self._font_height = self._font.get_linesize()
 
 	def draw(self):
-		self.screen.fill((0,0,0))
-		text = self.font.render(GameOver.GAME_OVER_TEXT, True, (255, 0, 0))
-		self.screen.blit(text, ((self.width - text.get_width()) / 2, (self.height - self.font_height) / 2))
+		""" Draws on the screen.
+
+		This method draws on the screen.
+		"""
+		self._screen.fill((0, 0, 0))
+		text = self._font.render(GameOver.GAME_OVER_TEXT, True, (255, 0, 0))
+		self._screen.blit(text, ((self._width - text.get_width()) / 2, (self._height - self._font_height) / 2))
 
 	def key_down(self, key):
+		""" Handles key down events.
+
+		This method handles key down events.
+		The pressing of the escape button results in the changing to the menu screen.
+
+		Args:
+			key: The key event information provided by pygame.
+		"""
 		if key == K_ESCAPE:
-			self.window.switch(window.Window.MENU)
+			self._window.switch(window.Window.MENU)
 
 	def key_up(self, key):
+		""" Handles key up events.
+
+		This method handles key up events.
+		These events will be ignored.
+
+		Args:
+			key: The key event information provided by pygame.
+		"""
 		pass
