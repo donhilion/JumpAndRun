9f484c31146ac2b02a89e4e634a40bdb5512b0e2
Donhilion <donhilion@googlemail.com>
2013-10-14 11:20:53 +0200

    Commented menu class.

--- graphics/menu.py
+++ graphics/menu.py
@@ -1,63 +1,121 @@
 import pygame
 from pygame.locals import *
-import logging
-
 from resources.pictures.picture_manager import PictureManager
 import window
 
+__author__ = 'Donhilion'
+
+
 class MenuEntry(object):
+	""" The menu entry class.
+
+	An instance of this class represents a menu entry.
+
+	Attributes:
+		text: The text of the entry.
+		action: The action to perform when the entry is selected.
+	"""
 
 	def __init__(self, text, action):
 		self.text = text
 		self.action = action
 
-class Menu(object):
-	'''
-	'''
 
+class Menu(object):
+	""" The menu class.
+
+	An instance of this class represents the menu screen.
+
+	Attributes:
+		_screen: The screen to draw on.
+		_width: The width of the window.
+		_height: The height of the window.
+		_window: The surrounding window.
+		_bg: The background picture.
+		_font: The font to use.
+		_font_height: The height of the used font.
+		_selected: The index of the selected menu entry.
+	"""
+
+	# The menu entries to show.
 	ENTRIES = (MenuEntry("Start", None), MenuEntry("Options", None),
-		MenuEntry("Ende", quit))
+			   MenuEntry("Ende", quit))
 
 	def __init__(self, screen, width, height, window):
-		self.screen = screen
-		self.width = width
-		self.height = height
-		self.window = window
-		self.bg = PictureManager.MANAGER.loaded["menuBackground.png"]
-		self.bg = pygame.transform.scale(self.bg, (width, height))
-		self.font = pygame.font.SysFont("arial", 24)
-		self.font_height = self.font.get_linesize()
-		self.selected = 0
+		""" Generates a new instance of this class.
+
+		Generates a new instance of this class and sets the field information.
+
+		Args:
+			screen: The screen to draw on.
+			width: The width of the window.
+			height: The height of the window.
+			window: The surrounding window.
+		"""
+		self._screen = screen
+		self._width = width
+		self._height = height
+		self._window = window
+		self._bg = PictureManager.MANAGER.loaded["menuBackground.png"]
+		self._bg = pygame.transform.scale(self._bg, (width, height))
+		self._font = pygame.font.SysFont("arial", 24)
+		self._font_height = self._font.get_linesize()
+		self._selected = 0
 
 		Menu.ENTRIES[0].action = self.to_game
 
 	def draw(self):
-		self.screen.blit(self.bg, (0,0))
-		y = 0.5 * (self.height - len(Menu.ENTRIES) * (self.font_height + 5))
+		""" Draws the menu.
+
+		This method draws the menu on the screen.
+		"""
+		self._screen.blit(self._bg, (0, 0))
+		y = 0.5 * (self._height - len(Menu.ENTRIES) * (self._font_height + 5))
 		i = 0
 		for entry in Menu.ENTRIES:
-			if self.selected == i:
-				text = self.font.render(entry.text, True, (255, 255, 0))
+			if self._selected == i:
+				text = self._font.render(entry.text, True, (255, 255, 0))
 			else:
-				text = self.font.render(entry.text, True, (150, 150, 0))
-			self.screen.blit(text, ((self.width - text.get_width()) / 2, y))
-			y += self.font_height + 5
+				text = self._font.render(entry.text, True, (150, 150, 0))
+			self._screen.blit(text, ((self._width - text.get_width()) / 2, y))
+			y += self._font_height + 5
 			i += 1
 
 	def key_down(self, key):
-		if key == K_DOWN and self.selected < len(Menu.ENTRIES) - 1:
-			self.selected += 1
-		elif key == K_UP and self.selected > 0:
-			self.selected -= 1
+		""" Handles key down events.
+
+		This method handles key down events.
+		The pressing of the up or down key changes the selected menu entry.
+		The pressing of the return key performs the action corresponding to the selected menu entry.
+
+		Args:
+			key: The key event information provided by pygame.
+		"""
+		if key == K_DOWN and self._selected < len(Menu.ENTRIES) - 1:
+			self._selected += 1
+		elif key == K_UP and self._selected > 0:
+			self._selected -= 1
 		elif key == K_RETURN:
-			entry = Menu.ENTRIES[self.selected]
+			entry = Menu.ENTRIES[self._selected]
 			if entry.action is not None:
 				entry.action()
 
 
 	def key_up(self, key):
+		""" Handles key up events.
+
+		This method handles key up events.
+		These events will be ignored.
+
+		Args:
+			key: The key event information provided by pygame.
+		"""
 		pass
 
 	def to_game(self):
-		self.window.switch(window.Window.GAME)
+		""" Changes to the game.
+
+		This method changes to the game surface.
+		"""
+		self._window.switch(window.Window.GAME)
 		
